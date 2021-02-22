e1822a202b2a334b39e36d96790ebcd8ee59c081
ThreshMain <martinkos007@gmail.com>
2021-02-22 23:10:03 +0100

    Added JUnit 5 testing
    Added deploy settings option for maven project

--- src/test/java/ChatServerTest.java
+++ src/test/java/ChatServerTest.java
@@ -1,11 +1,39 @@
+import main.Main;
+import main.chat.service.ChatRoomService;
+import main.chat.service.MessageService;
+import main.chat.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class ChatServerTest {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+public class ChatServerTest {
+    @Autowired
+    UserService userService;
+    @Autowired
+    ChatRoomService chatRoomService;
+    @Autowired
+    MessageService messageService;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    public void notNull() {
+        Assertions.assertNotNull(userService);
+        Assertions.assertNotNull(chatRoomService);
+        Assertions.assertNotNull(messageService);
     }
 
+    @Test
+    public void testGetRooms() throws Exception {
+        this.mockMvc.perform(get("/room/all")).andDo(print()).andExpect(status().isOk());
+    }
 }
